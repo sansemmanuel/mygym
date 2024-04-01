8471185045dd9ffbe901a40d7c08150f7404b703
sansemmanuel <emmanuel.sansberoo@gmail.com>
2024-04-01 16:03:58 -0300

    asd

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-
+import { Analytics } from "@vercel/analytics/react"
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
       {children}
       <Footer />
+      <Analytics></Analytics>
     </>
   );
 };
