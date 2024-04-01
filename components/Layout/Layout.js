f208a2a6b0b9e1e2b5a7b4791b2f65fd47f7f796
sansemmanuel <emmanuel.sansberoo@gmail.com>
2024-04-01 16:08:17 -0300

    fix

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import { Analytics } from "@vercel/analytics/"
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
       {children}
       <Footer />
-      <Analytics></Analytics>
     </>
   );
 };
